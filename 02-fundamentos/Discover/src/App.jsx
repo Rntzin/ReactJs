ea0db6c853e45384c00501a1959f7d6bb5124113
André Emanuel <kinglemoyra@gmail.com>
2024-08-12 13:19:36 -0300

    Aplicando Key/States

--- 02-fundamentos/Discover/src/App.jsx
+++ 02-fundamentos/Discover/src/App.jsx
@@ -1,15 +1,44 @@
+import { useState } from 'react';
+import { Card } from './components/Index'
+
 import './appmodule.css';
 
 export function App() {
+  const [studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent(){
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString('pt-br')
+    };
+
+    setStudents(prevState => [...prevState, newStudent]);
+  }
+
   return (
     <div className='container'>
       <h1>
-        Lista de presença
+        Lista de Presença
       </h1>
-      <input type="text" placeholder="Digite o nome"/>
-      <button type="button">
+
+      <input 
+        type="text" 
+        placeholder="Digite o nome"
+        onChange={e => setStudentName(e.target.value)}
+        />
+
+      <button type="button" onClick={handleAddStudent}>
         Adicionar
       </button>
+        {
+          students.map(student => (
+            <Card 
+              key={student.time}
+              name={student.name} 
+              time={student.time}/>
+          ))
+        }
     </div>
   )
 }
---
+++ 02-fundamentos/Discover/src/components/Index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+export function Card({ name, time }){
+    return(
+        <div className='card'>
+            <strong>
+                {name}
+            </strong>
+            <small>
+                {time}
+            </small>
+        </div>
+    )
+}
